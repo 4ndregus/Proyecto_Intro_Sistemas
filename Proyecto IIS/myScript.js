5daa3e15f8c28140993afefe9e400fc40def1291
4ndregus <63918420+4ndregus@users.noreply.github.com>
2020-05-02 07:28:00 -0600

    Proyecto final Intro Sistemas
    Documentos proyecto final página web

---
+++ Proyecto IIS/myScript.js
@@ -0,0 +1,16 @@
+var tabButtons = document.querySelectorAll(".container .buttonContainer button");
+var tabPanels = document.querySelectorAll(".container .tabPanel");
+
+function showPanel(panelIndex,colorCode){
+	tabButtons.forEach(function(node){
+		node.style.backgroundColor="";
+		node.style.color="";
+	});
+	tabButtons[panelIndex].style.backgroundColor=colorCode;
+	tabButtons[panelIndex].style.color="white";
+	tabPanels.forEach(function(node){
+		node.style.display="none";
+	});
+	tabPanels[panelIndex].style.display="block";
+	tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
